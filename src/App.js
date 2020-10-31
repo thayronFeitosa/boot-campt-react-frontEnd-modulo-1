47235b1f053aa9c66f6b16d9e33c46c6c3a0553f
thayron <thayronf3@gmail.com>
2020-10-31 09:44:06 -0300

    iniciando o projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div>
+           ola
+        </div>
+
+
+    );
+}
+
+export default App
