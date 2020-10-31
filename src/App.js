0e54967b022b3f1e7148b53df84a25fda392b0ee
thayron <thayronf3@gmail.com>
2020-10-31 12:40:05 -0300

    consumindo dados da api no font-end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css'
-import Header from './components/Header';
-import backgroundImage from './assets/background.jpeg'
+import Header from './components/Header'
+import api from './services/api'
+
 /**
  * Componente
  * Propriedade
  * Estado e Imutabilidade
  */
 function App() {
-  const  [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
-
+  const  [projects, setProjects] = useState([]);
+  useEffect(()=> {
+    api.get('projects').then( response =>{
+      setProjects(response.data)
+    })
+  },[])
   // useState retorna um array de 2 posições 
   /**
    * 1. Variavewl com o seu valor inicial
@@ -23,10 +28,9 @@ function App() {
   }
   return (
     <>
-      <img src={backgroundImage} alt=""/>
       <Header title="Projects" />
       <ul>
-        {projects.map(projects => <li key={projects}>{projects}</li>)}
+        {projects.map(projects => <li key={projects.id}>{projects.title}</li>)}
       </ul>
       <button type="button" onClick={handleAppProject} >adicionar projeto</button>
     </>
